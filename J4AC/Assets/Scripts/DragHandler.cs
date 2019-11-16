f0263453b4390f6b7ebdca6b3076dc234fa09cf7
cxc9597 <cxc9597@IGM2025-08>
2019-11-15 20:48:31 -0500

    Started UI

---
+++ J4AC/Assets/Scripts/DragHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    [SerializeField] private Transform parent;
+    [SerializeField] private Transform optionsToolbar;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        transform.SetParent(optionsToolbar);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parent);
+    }
+}
